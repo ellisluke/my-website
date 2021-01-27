2b56a72c0b24e888250407988b5715a0d68cfa04
lellis20 <44104190+lellis20@users.noreply.github.com>
2021-01-27 13:55:45 -0500

    first deployment/under construction

---
+++ public/static/construction.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#main-header").hide();
+    $("#main-header").slideToggle(1000);
+});
+
+setInterval(()=> {
+    $("#dots").html(setDots());
+}, 1000);
+
+var i = 0;
+var dots = ["",".",". .",". . ."];
+function setDots() {
+    i++;
+    return dots[i%4];
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+// import express from 'express';
+// import path from 'path';
+// IBM API KEY: 9pIqLd0dit7_aTAFwf4_IThwYyRsqr1Jeb128p1sEC_h
+
+/* Hosting commands
+cd into my-website
+!!!!!! ONE TIME SETUP: curl -sL https://ibm.biz/idt-installer | bash
+
+ibmcloud login -a cloud.ibm.com -r us-south -g Default
+ibmcloud ks cluster config --cluster c08gnvjd0hakn5e3nb10
+kubectl config current-context
+
+*/
+
+const express = require('express');
+const { dirname } = require('path');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'construction.html'));
+});
+
+app.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+
+
+app.listen(port, () => console.log("Listening on port " + port));
